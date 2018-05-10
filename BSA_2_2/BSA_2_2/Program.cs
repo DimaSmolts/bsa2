302a22c97c9a991bcdd8cf932d9730e260266b05
DimaSmolts <dima.smolts@gmail.com>
2018-05-10 18:43:35 +0300

    Hello BSA 2018!

---
+++ BSA_2_2/BSA_2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSA_2_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello Binary Studio Academy 2018!");
+			Console.ReadKey();
+		}
+	}
+}
