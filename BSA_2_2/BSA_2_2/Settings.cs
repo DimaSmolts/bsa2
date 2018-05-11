7ab042651558875d6285c9256dd840e16d61791b
DimaSmolts <dima.smolts@gmail.com>
2018-05-11 15:09:30 +0300

    Created class Settings

---
+++ BSA_2_2/BSA_2_2/Settings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSA_2_2
+{
+	public static class Settings
+	{
+		private static int timeOut;
+		private static Dictionary<int, string> priceList;
+		private static int parkingSpace;
+		private static double fine;
+
+		static Settings()
+		{
+			// initializing
+
+			timeOut = 3;
+
+			priceList = new Dictionary<int, string>();
+			priceList.Add(1, "motorcycle");
+			priceList.Add(2, "bus");
+			priceList.Add(3, "passenger car");
+			priceList.Add(5, "truck");			
+
+			parkingSpace = 100;
+
+			fine = 2.5;
+		}
+
+		public static int TimeOut
+		{
+			get
+			{
+				return timeOut;
+			}
+		}
+		public static Dictionary<int, string> PriceList
+		{
+			get
+			{
+				return priceList;
+			}
+		}
+		public static int ParkingSpace
+		{
+			get
+			{
+				return parkingSpace;
+			}
+		}
+		public static double Fine
+		{
+			get
+			{
+				return fine;
+			}
+		}
+	}
+}
